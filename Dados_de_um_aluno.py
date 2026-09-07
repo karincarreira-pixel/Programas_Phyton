f51edba300459691a67b6ed17b2e2c7c17426c72
Karina Fernanda <karincarreira@gmail.com>
2026-09-06 22:04:05 -0300

    Programa: Dados de um aluno

---
+++ Dados_de_um_aluno.py
@@ -0,0 +1,9 @@
+nome = input("Nome do aluno:")
+idade = input("Idade do aluno:")
+nota1 = float(input("Nota na primeira avaliação:"))
+nota2 = float(input("Nota na segunda avaliação:"))
+
+media = round((nota1 + nota2)/2)
+
+print("A média das avaliações do aluno é de ",media)
+
