497b001ec24d6e57aab61757e4822349c33830b4
Karina Fernanda <karincarreira@gmail.com>
2026-09-06 21:19:59 -0300

    Programa: Cálculo da área do terreno

---
+++ Area_de_um_terreno.py
@@ -0,0 +1,7 @@
+comprimento = float(input("Qual o comprimento do terreno?"))
+largura = float(input("Qual a largura do terreno?"))
+
+area = round(comprimento * largura,2)
+
+print("A área do terreno é de",area)
+
